dccc3be9b4fbf61ae7c4610c67732d62caf7c138
Shahrom <mshahromali@gmail.com>
2024-12-02 08:21:28 -0500

    reorder collection

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import SentientCard from "./SentientCard";
 
 const sentientData = [
   {
-    id: "chabot-1",
-    name: "Chabot",
+    id: "sam-1",
+    name: "S.A.M.",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    actionText: "Talk to Chabot",
-    onAction: () => console.log("Chabot interaction initiated"),
+    actionText: "Talk to S.A.M.",
+    onAction: () => console.log("S.A.M. interaction initiated"),
   },
   {
     id: "jaymort-1",
@@ -19,12 +19,12 @@ const sentientData = [
     onAction: () => console.log("Jay Mort interaction initiated"),
   },
   {
-    id: "sam-1",
-    name: "S.A.M.",
+    id: "chabot-1",
+    name: "Chabot",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    actionText: "Talk to S.A.M.",
-    onAction: () => console.log("S.A.M. interaction initiated"),
+    actionText: "Talk to Chabot",
+    onAction: () => console.log("Chabot interaction initiated"),
   },
 ];
 
