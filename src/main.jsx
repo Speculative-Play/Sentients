0f7788fc7dd456aae3a42f418a8c95b2d4fe03cd
Shahrom <mshahromali@gmail.com>
2025-01-14 14:49:45 -0500

    Hash routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,20 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
 import JayMort from "./JayMort/JayMort.jsx";
-import Chabot from "./Chabot/Chabot.jsx"
-import SAM from "./SAM/SAM.jsx"
+import Chabot from "./Chabot/Chabot.jsx";
+import SAM from "./SAM/SAM.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/chabot" element={<Chabot />} />
         <Route path="/jaymort" element={<JayMort />} />
         <Route path="/sam" element={<SAM />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
