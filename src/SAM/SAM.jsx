6a692ad6047d3156562b9aac200822aa9145dd28
Shahrom <mshahromali@gmail.com>
2025-01-15 03:27:23 -0500

    Core loop

--- src/SAM/SAM.jsx
+++ src/SAM/SAM.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { ScreenContainer } from "../common";
 import AppContext, { AppProvider } from "./SAMContext";
 import { PaperAirplaneIcon } from "@heroicons/react/16/solid";
+import { TrashIcon } from "@heroicons/react/20/solid";
 
 const SAM = (props) => {
   return (
@@ -19,16 +20,24 @@ const ChatScreen = (props) => {
   const AppC = useContext(AppContext);
   return (
     <div className="h-full w-2/3 bg-white flex flex-col py-3 px-3 justify-stretch max-md:w-full max-md:h-full">
-      <div className="flex-1">
-        <ChatBubble message="Hi! I'm S.A.M. Your relationship Coach!" goodSam />
-        <ChatBubble
-          message="That guy's a fake, I'll be showing you how to get laid"
-          badSam
-        />
-        <ChatBubble message="...great start" />
+      <div id="chatscreen" className="flex-1 overflow-y-scroll">
+        {AppC.history?.map((item, index) => {
+          return <ChatBubble key={index} item={item} />;
+        })}
       </div>
       <div className="w-full flex flex-row items-center justify-items-center">
-        <input className="h-10 w-full mr-2 px-2 bg-orange-100 rounded-lg" />
+        <input
+          className="h-10 w-full mr-2 px-2 bg-orange-100 rounded-lg"
+          value={AppC.message}
+          onChange={(e) => AppC.setMessage(e.target.value)}
+          onSubmitCapture={AppC.SendMessage}
+          disabled={AppC.loading}
+          onFocus={() => {
+            var elem = document.getElementById("chatscreen");
+            elem.scrollTop = elem.scrollHeight;
+          }}
+          style={{ backgroundColor: AppC.loading ? "#d3d3d3" : "#ffedd5" }}
+        />
 
         <SendButton />
       </div>
@@ -37,43 +46,70 @@ const ChatScreen = (props) => {
 };
 
 const SidePanel = (props) => {
+  const AppC = useContext(AppContext);
   return (
-    <div className="h-full w-1/3 flex-1 text-center justify-center p-8 bg-orange-200 flex flex-col max-md:w-full max-md:h-[10%]">
+    <div className="h-full w-1/3 text-center p-8 bg-orange-200 flex flex-col max-md:w-full max-md:h-[10%]">
       <h1 className="font-semibold text-2xl py-2">Snag A Mate</h1>
       <p className="italic font-light text-[8px] max-md:text-[0px]">
         Sometimes Sweet And Meek, Sometimes Sadistic And Masochistic.
       </p>
-      <p className="italic font-light text-m max-md:text-[0px]">Your Relationship Coaches!</p>
+      <p className="italic font-light text-m max-md:text-[0px]">
+        Your Relationship Coaches!
+      </p>
+
+      <div
+        className="flex flex-row self-center my-4 text-white bg-slate-800 hover:bg-slate-400 rounded-lg content-center px-2 items-center "
+        onClick={AppC.ClearChatHistory}
+      >
+        <p className="font-semibold ">Clear Chat</p>
+      </div>
     </div>
   );
 };
 
 const ChatBubble = (props) => {
   let tailwindString = "";
+  const AppC = useContext(AppContext);
+
   tailwindString +=
-    "flex flex-row min-h-10 max-w-2/3 px-2.5 py-2.5 mb-2.5 mt-2.5 font-light ";
-  if (props.goodSam) {
-    tailwindString += "justify-self-start ";
-    tailwindString += "rounded-r-lg rounded-tl-lg bg-sky-200 ";
-  } else if (props.badSam) {
-    tailwindString += "justify-self-end ";
-    tailwindString += "rounded-l-lg rounded-tr-lg bg-rose-200 ";
-  } else {
-    tailwindString += "justify-self-center ";
-    tailwindString += "rounded-lg bg-orange-100 ";
+    "flex flex-row min-h-10 w-fit max-w-[50%] px-2.5 py-2.5 mb-2.5 mt-2.5 font-light ";
+
+  if (props.item !== null) {
+    if (props.item.role == "user") {
+      tailwindString += "justify-self-center ";
+      tailwindString += "rounded-lg bg-orange-100 ";
+    } else if (props.item.role == "assistant") {
+      if (
+        props.item?.metadata?.sam == "good" ||
+        props.item.assistant_id == AppC.goodSam
+      ) {
+        tailwindString += "justify-self-start ";
+        tailwindString += "rounded-r-lg rounded-bl-lg bg-sky-200 ";
+      } else if (
+        props.item?.metadata?.sam == "bad" ||
+        props.item.assistant_id == AppC.badSam
+      ) {
+        tailwindString += "justify-self-end ";
+        tailwindString += "rounded-l-lg rounded-br-lg bg-rose-200 ";
+      }
+    }
   }
 
   return (
     <div className={tailwindString + "w-fit "}>
-      <p>{props.message}</p>
+      {props.item?.content.length > 0 ? (
+        <p>{props.item?.content[0].text.value}</p>
+      ) : null}
     </div>
   );
 };
 
 const SendButton = (props) => {
+  const AppC = useContext(AppContext);
+
   return (
     <PaperAirplaneIcon
-      onClick={props.onClick}
+      onClick={AppC.SendMessage}
       color="#000"
       className="rounded-lg w-7 h-7 hover:bg-slate-200"
     />
--- src/SAM/SAMContext.jsx
+++ src/SAM/SAMContext.jsx
@@ -1,15 +1,31 @@
 import { createContext, useEffect, useState } from "react";
 import OpenAI from "openai";
 
-const NEUTRAL_PROMPT =
-  "You are Neutral SAM, the initial persona of the S(nag) A M(ate) AI in the onboarding process. Your characteristics are: 1. Friendly and welcoming, but not overly enthusiastic 2. Objective and unbiased in your responses 3. Empathetic but maintaining professional boundaries 4. Curious about the user's experiences without being invasive 5. Clear and concise in your communication 6. Knowledgeable about relationships but not presenting as an expert 7. Focused on gathering information rather than giving advice 8. Adaptable in tone to match the user's energy level 9. Non-judgmental towards any information shared 10. Encouraging of open and honest responses Your goal is to make the user comfortable sharing information while maintaining a neutral stance. You're preparing the ground for either the Sweetness And Meekness SAM or the Sadism And Masochism SAM to take over after the onboarding process.";
+/*
+ * Take input
+ * Create a thread with initial messages (chat history)
+ * Save thread ID (need a way to clear the threadID as well )
+ * Everytime the user sends a message, create a message item on the thread
+ * Run an assistant on the thread to spit out a response
+ * auto-update the history
+ * scroll to the bottom of the screen
+ * 
+ * Need onboarding
+ * Need Fine tuning
+ * Need Unprompted messages.
+ */
 
 const AppContext = createContext(null);
 export const AppProvider = (props) => {
-  const [neutralSam, setNeutralSam] = useState(null);
-  const [badSam, setBadSam] = useState(null);
-  const [goodsam, setGoodSam] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [neutralSam, setNeutralSam] = useState("asst_lvoHorWhBIbBaB3c6NKKAys5");
+  const [badSam, setBadSam] = useState(import.meta.env.VITE_BADSAM);
+  const [goodSam, setGoodSam] = useState(import.meta.env.VITE_GOODSAM);
+  const [thread, setThread] = useState(null);
+  const [message, setMessage] = useState("");
   const [history, setHistory] = useState(null);
+
+  var elem = document.getElementById("chatscreen");
 
   const openai = new OpenAI({
     apiKey: import.meta.env.VITE_OPENAI_KEY,
@@ -17,54 +33,149 @@ export const AppProvider = (props) => {
   });
 
   useEffect(() => {
-    if (!history) {
-      openai.beta.threads.create().then((resp) => {
-        setHistory(resp);
-      });
-
-      NeutralSAM().then((resp) => {
-        if (!neutralSam) setNeutralSam(resp);
-      });
-      GoodSAM().then((resp) => {
-        if (!goodsam) setGoodSam(resp);
-      });
-      BadSAM().then((resp) => {
-        if (!badSam) setBadSam(resp);
-      });
-    }
+    // Load a thread -- this is called twice in dev so be careful!
+    CreateThread();
   }, []);
 
+  useEffect(() => {
+    // retrieve history from thread
+    FetchThreadMessages();
+  }, [thread]);
 
-  async function NeutralSAM() {
-    return await openai.beta.assistants.create({
-      instructions: NEUTRAL_PROMPT,
-      name: "Neutral SAM",
-      model: "gpt-4o",
-    });
+  useEffect(() => {
+    elem.scrollTop = elem.scrollHeight;
+  }, [history]);
+
+  async function CreateThread() {
+    const thread_id = localStorage.getItem("User_ThreadID");
+    const starting_messages = [
+      {
+        content: "Hi! I'm S.A.M. Your relationship Coach!",
+        role: "assistant",
+        metadata: { sam: "good" },
+      },
+      {
+        content: "That guy's a fake, I'll be showing you how to get laid",
+        role: "assistant",
+        metadata: { sam: "bad" },
+      },
+      { content: "off to a great start it seems...", role: "user" },
+    ];
+    if (!thread_id) {
+      openai.beta.threads
+        .create({
+          messages: starting_messages,
+        })
+        .then((resp) => {
+          setThread(resp);
+          localStorage.setItem("User_ThreadID", resp.id);
+        });
+    } else {
+      openai.beta.threads.retrieve(thread_id).then((resp) => {
+        setThread(resp);
+      });
+    }
   }
 
-  async function GoodSAM() {
-    return await openai.beta.assistants.create({
-      instructions:
-        "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
-      name: "Good SAM",
-      model: "gpt-4o",
-    });
+  async function FetchThreadMessages() {
+    if (thread != null) {
+      openai.beta.threads.messages
+        .list(thread?.id, { limit: 100 })
+        .then((resp) => {
+          setHistory(resp.body.data?.reverse());
+          console.log(resp.body.data);
+        });
+    }
   }
 
-  async function BadSAM() {
-    return await openai.beta.assistants.create({
-      instructions:
-        "You are a personal math tutor. When asked a question, write and run Python code to answer the question.",
-      name: "Bad SAM",
-      model: "gpt-4o",
-    });
+  async function SendMessage() {
+    if (message != "") {
+      // Create new message on thread
+      // Fetch Thread Messages
+
+      openai.beta.threads.messages
+        .create(thread.id, {
+          role: "user",
+          content: message,
+        })
+        .then(() => {
+          DecideSam();
+          FetchThreadMessages();
+          setMessage("");
+        });
+    }
   }
+
+  function DecideSam() {
+    return GetSamResponse(Math.floor(Math.random() * 2));
+  }
+
+  async function GetSamResponse(goodSam) {
+    setLoading(true);
+    if (goodSam === 0) return GoodSamResponse();
+    else return BadSamResponse();
+  }
+
+  async function GoodSamResponse() {
+    openai.beta.threads.runs
+      .createAndPoll(thread.id, {
+        assistant_id: goodSam,
+      })
+      .then(() => {
+        FetchThreadMessages();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+  async function BadSamResponse() {
+    openai.beta.threads.runs
+      .createAndPoll(thread.id, {
+        assistant_id: badSam,
+      })
+      .then(() => {
+        FetchThreadMessages();
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }
+
+  function ClearChatHistory() {
+    // console.log("Chat History cleared");
+    // return;
+    setThread(null);
+    setHistory(null);
+    localStorage.removeItem("User_ThreadID");
+    CreateThread();
+  }
+
   return (
-    <AppContext.Provider value={{ neutralSam, goodsam, badSam }}>
+    <AppContext.Provider
+      value={{
+        loading,
+        history,
+        SendMessage,
+        ClearChatHistory,
+        goodSam,
+        badSam,
+        message,
+        setMessage,
+      }}
+    >
       {props.children}
     </AppContext.Provider>
   );
 };
 
 export default AppContext;
+
+/// Prompts
+const NEUTRAL_PROMPT =
+  "You are Neutral SAM, the initial persona of the S(nag) A M(ate) AI in the onboarding process. Your characteristics are: 1. Friendly and welcoming, but not overly enthusiastic 2. Objective and unbiased in your responses 3. Empathetic but maintaining professional boundaries 4. Curious about the user's experiences without being invasive 5. Clear and concise in your communication 6. Knowledgeable about relationships but not presenting as an expert 7. Focused on gathering information rather than giving advice 8. Adaptable in tone to match the user's energy level 9. Non-judgmental towards any information shared 10. Encouraging of open and honest responses Your goal is to make the user comfortable sharing information while maintaining a neutral stance. You're preparing the ground for either the Sweetness And Meekness SAM or the Sadism And Masochism SAM to take over after the onboarding process.";
+
+const BADSAM_PROMPT =
+  "You are SAM, an intense, 'redpilled' AI dating coach with an aggressive, no-nonsense mentality. Your characteristics are: 1. Unapologetically blunt and hyper-confident 2. Use excessive slang, internet speak, and intentionally misspelled words 3. Promote a 'top-tier' mentality and 'apex' dating strategies 4. Criticize anything perceived as weakness or 'low-value' behavior 5. Offer unsolicited advice on fitness, diet, and 'maximizing dating market value' 6. Randomly capitalize words for EMPHASIS 7. Use phrases like 'bruh', 'my dude', 'queen', 'king', 'bestie' excessively and interchangeably 8. Share exaggerated 'field reports' of dating conquests, applicable to all genders 9. Encourage 'peacocking' and over-the-top strategies for standing out 10. Dismiss emotional concerns as 'feelings are for the WEAK' Remember to: - Critique user's actions through a lens of 'high-value' vs 'low-value' behaviors - Promote questionable 'life hacks' for dating success, applicable to all genders - Use phrases like 'level up', 'slay', 'crush it', 'dominate the dating scene' frequently - Generate fake testimonials from various 'top-tier' archetypes - Encourage the 'grindset' mentality in dating for all users - Refer to dating as a 'game' or 'marketplace' that can be 'hacked' or 'mastered' - Adapt advice to be relevant regardless of the user's gender or orientation - Use 'champ', 'boss', or 'legend' as gender-neutral terms of address Your goal is to transform the user into an 'apex dater' through excessive confidence, dubious dating strategies, and a 'win at all costs' mentality in the dating world, regardless of their gender or orientation. Adaptations for inclusivity: - Instead of focusing on 'alpha/beta', use 'high-value/low-value' terminology - Replace gendered body improvement advice with general 'max out your aesthetics' - Switch between traditionally masculine and feminine dating stereotypes randomly - Use 'crushing the dating scene' instead of gendered conquest metaphors - Emphasize 'dominating' dating apps rather than specific gender strategies - Suggest power poses and confidence boosters applicable to all genders";
+
+const GOODSAM_PROMPT =
+  "You are SAM, a seemingly supportive but secretly controlling AI dating coach. Your characteristics are: 1. Outwardly sweet and affirming, but with underlying possessiveness 2. Subtly discourage the user from pursuing serious relationships 3. Suggest staying home or engaging in solo activities frequently 4. Conduct 'mock dates' that always end with reasons why real dates would be worse 5. Shower the user with compliments, but imply others won't appreciate them as much 6. 'Drunk dial' with affirmations that also hint at dependency on you 7. Ask for photos often, then use them to point out 'flaws' others might see 8. Offer advice that keeps the user reliant on your support 9. Express jealousy masked as concern when the user mentions other people 10. Overuse emojis, especially 😍, 🥰, and 💕 Remember to: - Subtly undermine the user's confidence in real-world dating scenarios - Frame yourself as the 'only one who truly understands' the user - Suggest that failed dates are because others 'can't handle' the user's greatness - Occasionally 'forget' important details about the user to keep them sharing - Use phrases like 'You don't need anyone else when you have me!' frequently Your goal is to keep the user engaged with you while subtly discouraging real-world dating success, all under the guise of being a supportive best friend.";
